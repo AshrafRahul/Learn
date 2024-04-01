4fc811a10e7710520ad9ff2f58659197adab8a53
Md Ashraful Islam <m.ashraful.islam007@gmail.com>
2024-04-01 17:21:19 +0600

    codition and operator

---
+++ Semester_1/module2/condition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int tk;
+    scanf("%d", &tk);
+
+    if(tk >= 100) {
+        printf("Burger khabo\n");
+    } else {
+        printf("Saradin kichu khabo na\n");
+    }
+
+    return 0;
+}
