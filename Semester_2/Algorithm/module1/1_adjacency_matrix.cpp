0c2b76eb3fdd84d038d2703897ab890538ce3e96
Ashraful <m.ashraful.islam007@gmail.com>
2024-09-02 23:39:31 +0600

    graph

---
+++ Semester_2/Algorithm/module1/1_adjacency_matrix.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int mat[n][n];
+
+    memset(mat, 0, sizeof(mat));
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        mat[a][b] = 1;
+        mat[b][a] = 1;
+    }
+    if (mat[3][1] == 1)
+        cout << "connection ache" << endl;
+    else
+        cout << "connection nai" << endl;
+
+    return 0;
+}
